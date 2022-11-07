07b5f76f076463fe43574e4bfbc1cc5b944f08fc
Mauro Moreno <mmauromoreno1@gmail.com>
2022-11-07 20:30:53 -0300

    Added accelerometer to the ball

---
+++ Game Therapy/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    private bool isFlat = true;
+    private Rigidbody playerRB;
+    private float speed = 5f;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(180, 0, 0) * tilt * speed;
+        }
+
+        playerRB.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.blue);
+    }
+}
